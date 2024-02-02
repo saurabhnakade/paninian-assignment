40bd22453c96eeb3b78497112dbfbd7a273347a9
saurabhnakade <saurabh.nakade23@gmail.com>
2024-02-02 16:20:15 +0530

    projection code

--- fluid-simulaion.cpp
+++ fluid-simulaion.cpp
@@ -20,6 +20,7 @@ static void lin_solve(int b, float *x, float *x0, float a, float c, int iter, in
                         )) * cRecip;
             }
         }
+        // set bound
     }
 }
 
@@ -28,6 +29,37 @@ static void lin_solve(int b, float *x, float *x0, float a, float c, int iter, in
 static void diffuse(int b, float *x, float *x0, float diff, float dt, int iter, int n) {
     float a=dt*diff*(n-2)*(n-2);
     lin_solve(b, x, x0, a, 1 + 6 * a, iter, n);
+}
+
+// makes sure that the liquid is incompressible
+static void project(float *velocX, float *velocY, float *p, float *div, int iter, int n)
+{
+
+    for (int j = 1; j < n - 1; j++) {
+        for (int i = 1; i < n - 1; i++) {
+            div[getIndex(i, j, n)] = -0.5f*(
+                        velocX[getIndex(i+1, j  , n )]
+                    -velocX[getIndex(i-1, j  , n )]
+                    +velocY[getIndex(i  , j+1, n )]
+                    -velocY[getIndex(i  , j-1, n )]
+                )/n;
+            p[getIndex(i, j, n)] = 0;
+        }
+    }
+
+    //set bound
+    lin_solve(0, p, div, 1, 6, iter, n);
+    
+    for (int j = 1; j < n - 1; j++) {
+        for (int i = 1; i < n - 1; i++) {
+            velocX[getIndex(i, j, n)] -= 0.5f * (  p[getIndex(i+1, j, n)]
+                                            -p[getIndex(i-1, j, n)]) * n;
+            velocY[getIndex(i, j, n)] -= 0.5f * (  p[getIndex(i, j+1, n)]
+                                            -p[getIndex(i, j-1, n)]) * n;
+        }
+    }
+
+    //set bound
 }
 
 class Fluid{
